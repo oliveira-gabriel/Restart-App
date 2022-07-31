463972ed13cabc6bfd5c6677b86104d1f17a4ffa
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-07-30 22:36:25 -0300

    feat: add initial logic

--- Restart/Screens/ContentView.swift
+++ Restart/Screens/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 11 Pro")
     }
 }
