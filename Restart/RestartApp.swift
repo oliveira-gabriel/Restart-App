ab8a98b2436b5a9e924aadfddbc4bce687a359b8
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-07-30 22:09:49 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Gabriel Oliveira on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
